e22a0ae5e53c522d6dfacd77b1401db2f7bb098b
Uwe Jäger <uwej711@googlemail.com>
2012-02-23 14:23:42 +0100

    fix typo

--- symfony2/recipes/default.rb
+++ symfony2/recipes/default.rb
@@ -6,7 +6,7 @@ require_recipe "php"
 require_recipe "php::module_mysql"
 require_recipe "php::module_apc"
 require_recipe "php::module_sqlite3"
-require_recipe "php::mopule_curl"
+require_recipe "php::module_curl"
 require_recipe "apache2::mod_php5"
 require_recipe "gems"
 
